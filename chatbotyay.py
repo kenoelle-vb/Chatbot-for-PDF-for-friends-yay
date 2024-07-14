5d59c2be0d2b850b91901e728e6d9ccc74058d63
kenoelle-vb <knolel9001@gmail.com>
2024-07-14 09:12:20 +0700

    Update chatbotyay.py

--- chatbotyay.py
+++ chatbotyay.py
@@ -140,7 +140,7 @@ if password == richness or password == corrie or password == keno :
             finalanswer =  finalanswer.choices[0].message.content
 
         if password == keno :
-            question_answer = f"Answer the question of {prompt} only from the context of {data} and answer explain in detail"
+            question_answer = f"Answer the question of {prompt} from the context of {data} and answer explain in detail, and use various other knowledge that you have."
             finalanswer = client.chat.completions.create(messages=[{"role":"user", "content":question_answer,}],model="llama3-8b-8192")
             finalanswer =  finalanswer.choices[0].message.content
         
